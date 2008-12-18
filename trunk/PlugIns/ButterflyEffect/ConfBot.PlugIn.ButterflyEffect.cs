bc8569ca1d3016f213ae69b9f036146650e9055c
debe.mechero <debe.mechero@83c0c0ba-c21c-11dd-b16b-519e865567fa>
2008-12-18 23:38:51 +0000

    Bugfixes  - diversi bug nel parser  - bug nella sostituzione delle vocali

--- trunk/PlugIns/ButterflyEffect/ConfBot.PlugIn.ButterflyEffect.cs
+++ trunk/PlugIns/ButterflyEffect/ConfBot.PlugIn.ButterflyEffect.cs
@@ -10,18 +10,18 @@ using ConfBot.PlugIns;
 namespace ConfBot.PlugIns
 {
 	[PlugInAttribute]
-	public class ButterflyEffect: PlugIn 
+	public class ButterflyEffect: PlugIn
 	{
 		private string[] vocali = new string[5] {"a", "e", "i","o","u"};
-		private string[] vofocafalifi = new string[5]{"fa","fe","fi","fo","fu"};
+		private string[] vofocafalifi = new string[5]{"afa","efe","ifi","ofo","ufu"};
 		private bool Active = false;
 		
 		private string farfallizza(string msg)
 		{
-			string farfalledMsg = "";
+			string farfalledMsg = msg;
 			for (int idx=0; idx < vocali.Length; idx++)
 			{
-				farfalledMsg = msg.Replace(vocali[idx],vofocafalifi[idx]);
+				farfalledMsg = farfalledMsg.Replace(vocali[idx],vofocafalifi[idx]);
 			}
 			
 			return farfalledMsg;
@@ -34,58 +34,68 @@ namespace ConfBot.PlugIns
 		public override bool msgCommand(ref Message msg, ref string newMsg, out bool command)
 		{
 			command = false;
-			//potrebbe essere un comando
-			if (msg.Body.ToLower().StartsWith("/butterfly"))
-			{
-				command = true;
-				if (confObj.isAdmin(msg.From))
+			try {
+				
+				//potrebbe essere un comando
+				if (msg.Body.ToLower().StartsWith("/butterfly"))
 				{
-					//se più lungo significa che c'è il parametro del comando
-					if (msg.Body.Length >= 11)
+					command = true;
+					if (confObj.isAdmin(msg.From.Bare))
 					{
-						String tmpMsg = msg.Body.Remove(0, 11).Trim().ToLower();
-						switch (tmpMsg) {
-							case "on": 	Active = true;
-										confObj.j.Message(msg.From, "*Butterfly Activated*");
-										break;
+						//se più lungo significa che c'è il parametro del comando
+						if (msg.Body.Length >= 10)
+						{
+							String tmpMsg = msg.Body.Remove(0, 10).Trim().ToLower();
+							switch (tmpMsg) {
+									case "on": 	Active = true;
+												confObj.j.Message(msg.From, "*Butterfly Activated*");
+												break;
 
-							case "off":	Active = false;
-										confObj.j.Message(msg.From, "*Butterfly DeActivated*");
-										break;
-										
-							case "help":String helpString = "*butterfly* riscrive il tuo messaggio in alfabeto farfallino";
-										helpString += "*/Butterfly help*: aiuto\n";
-										helpString += "*/cleanmode [on|off]*: attiva/disattiva il cleanmode\n";
-										confObj.j.Message(msg.From, helpString);																		
-										break; 
-										
-							default:	if (Active) {
-											confObj.j.Message(msg.From, "_Butterfly is active_");
-										} else {
-											confObj.j.Message(msg.From, "_Butterfly is not active_");
-										}
-										break;
+									case "off":	Active = false;
+												confObj.j.Message(msg.From, "*Butterfly DeActivated*");
+												break;
+									
+									case "help":String helpString = "*butterfly* riscrive il tuo messaggio in alfabeto farfallino";
+												helpString += "*/Butterfly help*: aiuto\n";
+												helpString += "*/cleanmode [on|off]*: attiva/disattiva il cleanmode\n";
+												confObj.j.Message(msg.From, helpString);
+												break;
+									
+									default:	if (Active) {
+													confObj.j.Message(msg.From, "_Butterfly is active_");
+												} else {
+													confObj.j.Message(msg.From, "_Butterfly is not active_");
+												}
+												break;
+							}
+						}
+						else{
+							confObj.j.Message(msg.From, "Che dice?!");
 						}
 					}
+					else
+					{
+						confObj.j.Message(msg.From, Conference.NOADMINMSG);
+					}
+					
 				}
 				else
 				{
-					confObj.j.Message(msg.From, Conference.NOADMINMSG);
+					//non è un comando quindi devo crittografare il msg se il plugin è attivo
+					if (Active)
+					{
+						newMsg= msg.Body;
+						newMsg = farfallizza(newMsg);
+					}
 				}
 				
-			}
-			else
-			{
-				//non è un comando quindi devo crittografare il msg se il plugin è attivo
-				if (Active)
-				{
-					newMsg= msg.Body;
-					newMsg = farfallizza(newMsg);
-				}
+				return true;
+				
+			} catch (Exception e) {
+				confObj.LogMessageToFile(e.Message);
+				return false;
 			}
 			
-			return true;
-
 		}
 
 		public override string Help()
