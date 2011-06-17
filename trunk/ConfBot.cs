b13909281088c0a9cdfa78872f32427b86e9bd1d
debe.mechero <debe.mechero@83c0c0ba-c21c-11dd-b16b-519e865567fa>
2011-06-17 10:18:14 +0000

    Compilato con framework 3.5 Rimossi scarti del passato. aggiornata libreria agsXMPP.dll

--- trunk/ConfBot.cs
+++ trunk/ConfBot.cs
@@ -5,7 +5,7 @@
  * Time: 16.39
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */ 
+ */
 
 using System;
 using System.Threading;
@@ -19,7 +19,7 @@ namespace ConfBot
 	{
 		static Conference conf;
 		static JabberClient _jabberClient;
-	 	static Logger _logger;
+		static Logger _logger;
 		static ConfigManager _configMgr;
 		
 		const string CONFIGFILE	= "ConfBot.config";
@@ -28,16 +28,23 @@ namespace ConfBot
 		{
 			ExeConfigurationFileMap configFile = new ExeConfigurationFileMap();
 			configFile.ExeConfigFilename = args.Length > 0 ? args[0]: (CONFIGFILE) ;
-			_configMgr = new ConfigManager(ConfigurationManager.OpenMappedExeConfiguration(configFile, ConfigurationUserLevel.None));
-			_logger = new Logger(_configMgr.GetSetting("LogFile"));
-			_jabberClient = new JabberClient(_configMgr, _logger);
-			conf = new Conference(_configMgr,_jabberClient,_logger);
-			
-			Thread confThread = new Thread(conf.Run);
-			confThread.Start();
-			
-			while (!confThread.IsAlive);
-			confThread.Join();
+			if (File.Exists(configFile.ExeConfigFilename))
+			{
+				_configMgr = new ConfigManager(ConfigurationManager.OpenMappedExeConfiguration(configFile, ConfigurationUserLevel.None));
+				_logger = new Logger(_configMgr.GetSetting("LogFile"));
+				_jabberClient = new JabberClient(_configMgr, _logger);
+				conf = new Conference(_configMgr,_jabberClient,_logger);
+				
+				Thread confThread = new Thread(conf.Run);
+				confThread.Start();
+				
+				while (!confThread.IsAlive);
+				confThread.Join();
+			}
+			else
+			{
+				Console.WriteLine("Config File not Found: " + configFile.ExeConfigFilename);
+			}
 		}
 
 	}
